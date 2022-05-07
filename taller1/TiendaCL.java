38ddead9ea4a2007df0c6c57f50f3ffa4b6185bb
dragonnomada <dragonnomada123@gmail.com>
2022-05-07 11:43:43 -0500

    Taller 1

--- taller1/TiendaCL.java
+++ taller1/TiendaCL.java
@@ -4,12 +4,20 @@ import java.util.Scanner;
 
 class TiendaCL {
 
+    AdministradorProductosCL administradorProductosCL = new AdministradorProductosCL();
+    AdministradorVentaCL administradorVentaCL = new AdministradorVentaCL();
+    Scanner scanner = new Scanner(System.in);
+
     void mostrarMenu() {
         System.out.println("TiendaCL");
         System.out.println("-----------------");
         System.out.println("1. Administrador de Productos");
         System.out.println("2. Administrador de Venta");
         System.out.println("3. Salir");
+
+        int opcion = solicitarOpcion(scanner);
+
+        seleccionarOpcion(opcion);
     }
 
     int solicitarOpcion(Scanner scanner) {
@@ -32,20 +40,24 @@ class TiendaCL {
                 break;
             default:
                 System.out.println("Opción no válida");
+                mostrarMenu();
                 break;
         }
     }
 
     void abrirAdministradorProductos() {
-
+        administradorProductosCL.mostrarMenu();
+        mostrarMenu();
     }
 
     void abrirAdministradorVenta() {
-
+        administradorVentaCL.mostrarMenu();
+        mostrarMenu();
     }
 
     void salir() {
-
+        scanner.close();
+        System.out.println("Fin del programa");
     }
 
 }
